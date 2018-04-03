749bc73f269e4f2e526cee5b5794eb849fa3a33b
Jarold Wong <jaroldw@gmail.com>
2018-04-03 10:58:53 -0700

    Update dependencies

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './style.css';
+
+class App extends React.Component {
+  render() {
+    return <div>Hello World!</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -37,6 +37,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
-    })
+    }),
+    new webpack.optimize.UglifyJsPlugin()
   ]
 };
